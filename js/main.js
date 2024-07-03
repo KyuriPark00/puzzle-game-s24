e02badbb75dfa3edf172439651799eb74015ec62
KyuriPark00 <156356426+KyuriPark00@users.noreply.github.com>
2024-07-02 22:21:00 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,28 +1,54 @@
-console.log("Javascript is connected")
+console.log('Javascript is connected')
 
 //variables 
-const theButtons = document.querySelectorAll("#buttonHolder img");
-const puzzleBoard = document.querySelector(".puzzle-board")
+const theButtons = document.querySelectorAll('#buttonHolder img');
+const puzzleBoard = document.querySelector('.puzzle-board');
+const puzzlePieces = document.querySelectorAll('.puzzle-pieces img');
+const dropZones = document.querySelectorAll('.drop-zone');
+const puzzlePieceDiv = document.querySelector('.puzzle-pieces');
+let dragPiece;
 
-//functions
+
+//function
 function changeBGImage(event) {
-    //console.log("changeBGImage called");
-    //Method 1
-    //console.log(this.id);
-    //background-image: url("../images/backGround0,jpg");
-    puzzleBoard.style.backgroundImage = `url('../images/backGround${this.id}.jpg')`;
-
+    console.log('changeBGImage called');
+   
+    // Method 2 of changing puzzle using foreach loop and put back puzzle
+    const pos = ['topLeft', 'topRight', 'bottomLeft', 'bottomRight'];
+  
+    puzzlePieces.forEach((piece, index) => {
+      piece.src = `images/${pos[index]}${event.currentTarget.id}.jpg`;
+      puzzlePieceDiv.appendChild(piece);
+    });
+  
     //Method 2
     console.log(event.currentTarget.id);
-}
-
-// 드롭 존에 이미지 있을 시 반환.
-function dropped() {
+    puzzleBoard.style.backgroundImage = `url('../images/backGround${event.currentTarget.id}.jpg')`;
+  }
+  
+  function handlestartDrag() {
+    console.log('started dragging');
+    dragPiece = this;
+  }
+  
+  function handleOver(e) {
+    console.log('dragging over dropZone');
+    e.preventDefault();
+  }
+  
+  function dropped() {
+    // Method 1 check is there any image before the first puzzle drop
+    // if (this.children.length < 1) {
+    // this.appendChild(dragPiece);
+    //}
+  
+    //Method 2 check is there any image after the first puzzle drop
     if (this.children.length >= 1) {
-        return;
-      }
-      this.appendChild(dragPiece);
-}
+      return;
+    }
+    this.appendChild(dragPiece);
+  }
+  
 
 //eventListeners
-theButtons.forEach(button => button.addEventListener("click",changeBGImage));
+theButtons.forEach(button => button.addEventListener('click',changeBGImage));
