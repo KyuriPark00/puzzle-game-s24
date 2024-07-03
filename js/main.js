611d74420d31f5ac05fbaaeaabd243920d36eb51
KyuriPark00 <156356426+KyuriPark00@users.noreply.github.com>
2024-07-03 00:40:47 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,50 +1,79 @@
-console.log('Javascript is connected')
+console.log("Javascript is connected");
 
-//variables 
-const theButtons = document.querySelectorAll('#buttonHolder img');
-const puzzleBoard = document.querySelector('.puzzle-board');
-const puzzlePieces = document.querySelectorAll('.puzzle-pieces img');
-const dropZones = document.querySelectorAll('.drop-zone');
-const puzzlePieceDiv = document.querySelector('.puzzle-pieces');
-let dragPiece;
+// Variables
+const theButtons = document.querySelectorAll("#buttonHolder img");
+const puzzleBoard = document.querySelector(".puzzle-board");
+const puzzlePieces = document.querySelectorAll(".puzzle-pieces img");
+const dropZones = document.querySelectorAll(".drop-zone");
+const puzzlePieceDiv7 = document.querySelector(".puzzle-pieces");
 
+const resetButton = document.getElementById("resetBut");
+let draggedPiece;
+let currentPuzzle = 0;
 
-//function
+console.log(theButtons);
+console.log(puzzleBoard);
+
+// Functions
 function changeBGImage(event) {
-    console.log('changeBGImage called');
-   
-    // Method 2 of changing puzzle using foreach loop and put back puzzle
-    const pos = ['topLeft', 'topRight', 'bottomLeft', 'bottomRight'];
-  
-    puzzlePieces.forEach((piece, index) => {
-      piece.src = `images/${pos[index]}${event.currentTarget.id}.jpg`;
-      puzzlePieceDiv.appendChild(piece);
-    });
-  
-    //Method 2
+    console.log("changeBGimage called");
     console.log(event.currentTarget.id);
-    puzzleBoard.style.backgroundImage = `url('../images/backGround${event.currentTarget.id}.jpg')`;
-  }
-  
-  function handlestartDrag() {
-    console.log('started dragging');
-    dragPiece = this;
-  }
-  
-  function handleOver(e) {
-    console.log('dragging over dropZone');
+    puzzleBoard.style.backgroundImage = `url('./images/backGround${event.currentTarget.id}.jpg')`;
+    const puzzleId = event.currentTarget.id;
+
+    const pieceNames = ['topLeft', 'topRight', 'bottomLeft', 'bottomRight'];
+    puzzlePieces.forEach((piece, index) => {
+        piece.src = `./images/${pieceNames[index]}${puzzleId}.jpg`;
+    });
+
+    resetPieces();
+}
+
+// Start of dragging a puzzle piece
+function handleStartDrag() {
+    console.log(`started dragging ${this}`);
+    draggedPiece = this;
+}
+
+// Let piece to be dragged over drop zone
+function handleOver(e) {
     e.preventDefault();
-  }
-  
-  function dropped() {
-    // Method 1 check is there any image before the first puzzle drop
-    // if (this.children.length < 1) {
-    // this.appendChild(dragPiece);
-    //}
-  
-    //Method 2 check is there any image after the first puzzle drop
+    console.log("Dragged Over");
+}
 
-  
+// Drop the piece into drop zone
+function handleDrop(e) {
+    e.preventDefault();
+    console.log("Dropped");
 
-//eventListeners
-theButtons.forEach(button => button.addEventListener('click',changeBGImage));
+    if (this.children.length > 0) {
+        console.log("Drop zone took placed");
+        return;
+    }
+
+    this.appendChild(draggedPiece);
+}
+
+// Reset puzzle pieces to their original places
+function reset() {
+    console.log("User reset the puzzle.");
+    puzzlePieces.forEach(piece => {
+        puzzlePieceDiv7.appendChild(piece);
+    });
+}
+
+function resetPieces() {
+    puzzlePieces.forEach(piece => document.querySelector('.puzzle-pieces').appendChild(piece));
+}
+
+// Event Listeners
+theButtons.forEach(button => button.addEventListener("click", changeBGImage));
+
+puzzlePieces.forEach(piece => piece.addEventListener("dragstart", handleStartDrag));
+
+dropZones.forEach(zone => zone.addEventListener("dragover", handleOver));
+
+dropZones.forEach(zone => zone.addEventListener("drop", handleDrop));
+
+resetButton.addEventListener("click", reset);
+resetButton.addEventListener('click', resetPieces);
